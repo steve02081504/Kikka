dba73d56decf35b82a3e6652b7c56fab66303284
steve green <steve02081504@EDEN.fukwold>
2021-04-07 09:08:23 +0800

    Update

---
+++ clean-git-objs.sh
@@ -0,0 +1,21 @@
+git-clean-objs
+
+cd other
+git-clean-objs
+cd ..
+
+cd Taromati2
+git-clean-objs
+
+cd balloon
+git-clean-objs
+cd ..
+cd ghost
+git-clean-objs
+cd ..
+cd shell
+git-clean-objs
+cd ..
+
+cd ..
+
